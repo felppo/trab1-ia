38d967f28a2f66ca1bc3e301d99e49ddc9243efd
felppo <lippe4321@gmail.com>
2025-06-01 11:02:04 -0400

    final version

---
+++ bot_porta/knn.py
@@ -0,0 +1,30 @@
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import normalize
+from deepface import DeepFace
+
+#---------------------------------------------------------------------------------------------------------------
+
+x_train = np.loadtxt("treino_emb.txt")
+x_train_norm = normalize(x_train)
+y_train = np.loadtxt("treino_img.txt", dtype=str)
+
+model = KNeighborsClassifier(n_neighbors=6, weights="distance", metric="euclidean")
+model.fit(x_train_norm, y_train)
+
+#---------------------------------------------------------------------------------------------------------------
+
+#Processa a imagem recebida e salva os dados do reconhecimento facial
+def proc_received_photo(photo):
+    try:
+        embedding = DeepFace.represent(img_path=photo, model_name='Facenet512', enforce_detection=True)[0]['embedding']
+    except Exception:
+        return -1
+
+    emb_norm = normalize([embedding])
+    dist, index = model.kneighbors(emb_norm, n_neighbors=3)
+
+    if dist[0][0] > 0.5: #pessoa desconhecida
+        return 0
+    else: #pessoa conhecida
+        return 1
---
+++ bot_porta/telegram_bot.py
@@ -0,0 +1,33 @@
+import telebot
+import os
+import knn
+
+API_TOKEN = '8120327194:AAHi9btX1rpL2ERHc7QBwzBI_6w4yn7WyIw'
+bot = telebot.TeleBot(API_TOKEN)
+
+
+@bot.message_handler(func=lambda m: True)
+def welcome(message):
+    bot.reply_to(message, "não tô a fim de papo. manda logo uma foto pra eu te liberar.")
+
+
+@bot.message_handler(content_types=['photo'])
+def photo_received(img):
+    fileID = img.photo[-1].file_id
+    fileInfo = bot.get_file(fileID)
+    downloaded_file = bot.download_file(fileInfo.file_path)
+
+    with open("photo.jpg", 'wb') as new_file:
+        new_file.write(downloaded_file)
+
+    check = knn.proc_received_photo("photo.jpg")
+    os.remove("photo.jpg")
+    if check == -1:
+        bot.reply_to(img, "poh, manda uma foto melhorzinha")
+    elif check == 1:
+        bot.reply_to(img, "pode entrar, chefia.")
+    elif check == 0:
+        bot.reply_to(img, "vaza daqui antes que eu chame a polícia")
+
+
+bot.polling()
